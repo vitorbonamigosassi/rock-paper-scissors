bff65e7537aa17d4374da3dfd3abc07081ff6d40
Vitor Bonamigo Sassi <vitorbonamigosassi@gmail.com>
2024-04-22 16:30:33 -0300

    Add player input

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,31 @@
 console.clear();
 const choice = ["Rock","Paper","Scissors"];
 const comp = document.getElementById("computerChoice");
+const player = document.getElementById("playerChoice");
+
+let playerChoice = "";
 
 function getComputerChoice() {
     let chosen = choice[Math.floor(Math.random() * choice.length)];
     return comp.textContent = "" + chosen;
 }
-getComputerChoice()
+getComputerChoice();
+
+function chooseRock() {
+    playerChoice = "Rock";
+    player.textContent = "" + playerChoice;
+}
+
+function choosePaper() {
+    playerChoice = "Paper";
+    player.textContent = "" + playerChoice;
+}
+
+function chooseScissors() {
+    playerChoice = "Scissors";
+    player.textContent = "" + playerChoice;
+}
+
+// function getPlayerChoice(choice[option]) {
+
+// }
